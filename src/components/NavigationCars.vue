4e422a14889c1a020efc35150587c52fd29bfdca
ChristianReyes <christianquimporeyes@gmail.com>
2022-10-05 22:39:39 +0800

    header

--- src/components/NavigationCars.vue
+++ src/components/NavigationCars.vue
@@ -3,9 +3,10 @@
 </script>
 
 <template>
-<header class="hidden md:flex">
-    <div>logo</div>
-    <nav class="flex">
+<header class="hidden lg:flex justify-center items-center space-x-5 w-screen h-16">
+    <div class="w-24 h-10 bg-green-300">logo</div>
+    <nav class="flex justify-around w-2/3  text-white text-m font-bold">
+    <!---v-for -->
         <div>Home</div>
         <div>Buy Vehicle</div>
         <div>Sell Vehicle</div>
@@ -13,9 +14,9 @@
         <div>Help</div>
         <div>Contact Us</div>
     </nav>
-    <div class="flex">
-        <div>Register</div>
-        <div>Login</div>
+    <div class="flex space-x-4">
+        <div class="w-24 h-10 bg-orange-300">Register</div>
+        <div class="w-24 h-10 bg-cyan-400">Login</div>
     </div>
 </header>
 </template>

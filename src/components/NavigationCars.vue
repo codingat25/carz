0b64297ad533b383f3fe88d73dc2cfd4f79984ca
ChristianReyes <christianquimporeyes@gmail.com>
2022-10-13 21:45:28 +0800

    styling

--- src/components/NavigationCars.vue
+++ src/components/NavigationCars.vue
@@ -16,12 +16,6 @@
         <div><a href="#" class="hover:text-primaryButton">Help</a></div>
         <div><a href="#" class="hover:text-primaryButton">Contact Us</a></div>
     </nav>
-    <nav class="flex lg:hidden">
-        <div>X</div>
-    </nav>
-    <div class="hidden lg:flex space-x-4">
-        <div class="flex justify-center items-center w-32 h-8 bg-primaryButton text-white rounded-lg">Register</div>
-        <div class="flex justify-center items-center w-32 h-8 bg-tertiaryButton text-white rounded-lg">Login</div>
-    </div>
+
 </header>
 </template>

ffd85acb71cddac7698397dbcd7bf564dd504f13
Christian Jay Reyes <christianjay.reyes002@deped.gov.ph>
2022-10-24 21:33:01 +0800

    navigation

--- src/components/NavigationCars.vue
+++ src/components/NavigationCars.vue
@@ -14,17 +14,17 @@ const navLinks =
 
 <template>
 <div class="flex justify-center w-full h-hero-height pt-3 bg-desktop-hero bg-bottom bg-cover">
-	<div class="flex justify-center w-full lg:w-4/5 h-16 border border-green-500">
-		<div class="flex justify-center w-1/6 border border-red-500">
-			<img src="../assets/img/HeaderLogo.png" alt="website logo" class="h-16">
+	<div class="flex justify-around lg:justify-center w-full lg:w-4/5 h-16 space-x-5">
+		<div class="flex justify-center w-32 lg:w-1/6">
+			<img src="../assets/img/HeaderLogo.png" alt="website logo" class="hfull w-full">
 		</div>
-		<nav class="hidden md:flex justify-around items-center w-3/6 border border-red-500 ">
+		<nav class="hidden md:flex justify-around items-center w-3/6 space-x-3">
 			<a v-for="([title, path]) in navLinks" :key="path" :href="path" 
-			class="text-l font-bold font-sans text-white hover:text-primaryButton border border-red-500">
+			class="text-l font-bold font-sans text-white hover:text-primaryButton">
 				{{title}}
 			</a>
 		</nav>
-		<div class="flex justify-center items-center h-16 w-2/6 text-white">
+		<div class="hidden lg:flex justify-center space-x-5 items-center h-16 w-2/6 text-white">
 			<div class="flex justify-center w-1/3 rounded-md bg-primaryButton"><a href="#">Register</a></div>
 			<div class="flex justify-center w-1/3 rounded-md bg-tertiaryButton"><a href="#">Login</a></div>
 		</div>

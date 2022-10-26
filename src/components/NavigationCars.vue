1917fac696a2771e8d5e8e32dd9abedfd4b5f1a0
Christian Jay Reyes <christianjay.reyes002@deped.gov.ph>
2022-10-26 10:25:03 +0800

    header

--- src/components/NavigationCars.vue
+++ src/components/NavigationCars.vue
@@ -11,11 +11,11 @@ const navLinks =
 	]
 
 </script>
-
+<!-- NAVIGATION -->
 <template>
-<div class="flex justify-center h-screen w-screen lg:h-hero-height lg:w-full pt-2 bg-mobile-hero md:bg-desktop-hero bg-cover bg-right md:bg-center">
-	<div class="flex justify-between md:justify-center w-11/12 h-16">
-		<div class="flex items-center shrink-0 w-32 h-16">
+<div class="flex flex-col justify-start items-center h-screen w-screen lg:h-hero-height lg:w-full pt-2 bg-mobile-hero md:bg-desktop-hero bg-cover bg-right md:bg-center">
+	<div class="flex justify-between md:justify-center w-11/12 h-16 border border-red-500">
+		<div class="flex items-center shrink-0 w-32 h-16 ml-4">
 			<img src="../assets/img/HeaderLogo.png" alt="website logo" class="w-full">
 		</div>
 		<nav class="hidden md:flex justify-around items-center w-full lg:w-4/6 text-sm h-16 space-x-2">
@@ -29,10 +29,18 @@ const navLinks =
 			<div class="flex justify-center w-1/3 p-1 rounded-md bg-tertiaryButton"><a href="#">Login</a></div>
 		</div>
 
-		<div>
-
+		<div class="flex justify-center items-center md:hidden w-16 h-16 text-white">
+			<a href="">
+				<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+			</svg>
+			</a>
 		</div>
 	</div>	
+	<div class="w-11/12 h-full border border-red-500">
+		<p class="text-white">WELCOME TO</p>
+		<p class="text-white font-extrabold text-4xl">CARBID360</p>
+	</div>
 </div>
 
 </template>

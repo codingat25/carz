7be9f4af440b4346b9294ae2eea3721a526f8b4a
Christian Jay <christianquimporeyes@gmail.com>
2022-11-03 16:35:46 +0800

    cars

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -3,22 +3,29 @@ import { ref } from 'vue';
 </script>
 
 <template>
-    <div class="flex flex-col justify-center items-center h-[674px] border border-green-500">
-        <div class="flex justify-center items-end h-1/5 w-full border border-red-500 ">
-            <h1 class="text-primaryButton text-3xl font-semibold">BUY AND SELL CARS MORE EASILY</h1>
+    <div class="flex flex-col justify-start items-start pt-10  h- border border-green-500">
+        <div class="flex justify-center w-full border border-red-500 ">
+            <h1 class="text-primaryButton text-xl md:text-3xl font-semibold">BUY AND SELL CARS MORE EASILY</h1>
         </div>
        
-        <div class="flex justify-around items-center flex-col md:flex-row w-full h-3/5 border border-red-500">
+        <div class="flex flex-col justify-around items-center  md:flex-row w-full h-3/5 border border-yellow-500">
            
-           <div class="flex justify-center space-x-4 border border-yellow-500">
-            <div class="h-full py-2 w-3/4 border border-red-500 rounded-full"></div>
-            <div class="w-64 h-72 border border-green-500 ">
-                <h2>Shipping to any states</h2>
-            </div>
+            <div class="flex justify-between h-full w-1/3 border border-red-500">
+                <div class="w-2/6 h-2/6 border border-blue-500"></div>
+                <div class="w-3/5 h-full border border-green-500"></div>
            </div>
 
-           
+           <div class="flex justify-between h-full w-1/3 border border-red-500">
+                <div class="w-2/6 h-2/6 border border-blue-500"></div>
+                <div class="w-3/5 h-full border border-green-500"></div>
+           </div>
 
+           <div class="flex justify-between h-full w-1/3 border border-red-500">
+                <div class="w-2/6 h-2/6 border border-blue-500"></div>
+                <div class="w-3/5 h-full border border-green-500"></div>
+           </div>
         </div>
+
+        
     </div>
 </template>

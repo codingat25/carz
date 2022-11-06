1d3ce47636568dda86194a4a72c6ef7a6d8a6f07
Christian Jay <christianquimporeyes@gmail.com>
2022-11-06 21:46:00 +0800

    peste!

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -3,7 +3,35 @@ import { ref } from 'vue';
 </script>
 
 <template>
-    <div class="h-services-height w-full border border-red-500">
-        
+    <div class="flex flex-col h-auto w-full border border-red-500">
+        <div class="flex justify-center items-center w-full h-auto border border-red-500">
+            <h1 class="text-2xl md:text-3xl py-10 text-primaryColor font-extrabold">
+                BUY AND SELL CARS MORE EASILY
+            </h1>
+        </div>
+
+        <div class="flex flex-col md:flex-row">
+            <div class="flex flex-col md:flex-row justify-center">
+                <div class="w-28 h-28 bg-payments"></div>
+                <div>
+                    <h2 class="text-xl text-blue-700">header here</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui provident corrupti vitae eveniet beatae placeat, facere adipisci. Non atque dolorem veritatis! Unde maxime tempora ipsa a ullam, molestias omnis distinctio.</p>
+                </div>
+            </div>
+            <div class="flex flex-col md:flex-row justify-center">
+                <div class="w-28 h-28 bg-payments"></div>
+                <div>
+                    <h2 class="text-xl text-blue-700">header here</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui provident corrupti vitae eveniet beatae placeat, facere adipisci. Non atque dolorem veritatis! Unde maxime tempora ipsa a ullam, molestias omnis distinctio.</p>
+                </div>
+            </div>
+            <div class="flex flex-col md:flex-row justify-center">
+                <div class="w-28 h-28 bg-payments"></div>
+                <div>
+                    <h2 class="text-xl text-blue-700">header here</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui provident corrupti vitae eveniet beatae placeat, facere adipisci. Non atque dolorem veritatis! Unde maxime tempora ipsa a ullam, molestias omnis distinctio.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>

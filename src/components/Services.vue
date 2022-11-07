a8402e6d7883cf470c14d9776c25ab9be9aa7f27
Christian Jay <christianquimporeyes@gmail.com>
2022-11-07 22:06:51 +0800

    grrr

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -3,11 +3,59 @@ import { ref } from 'vue';
 </script>
 
 <template>
-    <div class="flex flex-wrap w-full border border-red-500">
-        <div class="flex justify-center items-center w-full h-auto border border-red-500">
+    <div class="flex flex-wrap w-full">
+        <div class="flex justify-center items-center w-full h-auto">
             <h1 class="text-xl md:text-3xl py-10 text-primaryColor font-black">
                 BUY AND SELL CARS MORE EASILY
             </h1>
         </div>
+
+        
+        <div class="container mx-auto flex flex-col lg:flex-row w-full">
+
+            <div class="flex flex-col md:flex-row justify-around w-full lg:w-1/3 h-auto pb-10">
+                <div class="flex justify-center w-full md:w-auto">
+                    <div class="w-24 h-24 mb-5 md:mb-0 mr-0 md:mr-5  ">
+                      <img src="../assets/img/Payments.png" alt="shipping">
+                     </div>
+                </div>
+ 
+                <div class="w-full lg:w-4/5">
+                    <h1 class="text-xl text-center lg:text-left text-blue-500">Shipping</h1>
+                    <p class="px-10 md:px-0 text-center md:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, repudiandae. Aspernatur quibusdam tempora reprehenderit modi quisquam, unde commodi laudantium, cupiditate neque tempore corporis! Possimus eius eum maxime iusto repudiandae veniam!</p>
+                </div>
+            </div>
+
+            <div class="flex flex-col md:flex-row justify-around w-full lg:w-1/3 h-auto pb-10">
+                <div class="flex justify-center w-full md:w-auto">
+                    <div class="w-24 h-24 mb-5 md:mb-0 mr-0 md:mr-5">
+                      <img src="../assets/img/Payments.png" alt="shipping">
+                     </div>
+                </div>
+ 
+                <div class="w-full lg:w-4/5">
+                    <h1 class="text-xl text-center lg:text-left text-blue-500">Shipping</h1>
+                    <p class="px-10 md:px-0 text-center md:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, repudiandae. Aspernatur quibusdam tempora reprehenderit modi quisquam, unde commodi laudantium, cupiditate neque tempore corporis! Possimus eius eum maxime iusto repudiandae veniam!</p>
+                </div>
+            </div>
+
+            <div class="flex flex-col md:flex-row justify-around w-full lg:w-1/3 h-auto pb-10">
+                <div class="flex justify-center w-full md:w-auto">
+                    <div class="w-24 h-24 mb-5 md:mb-0 mr-0 md:mr-5        ">
+                      <img src="../assets/img/Payments.png" alt="shipping">
+                     </div>
+                </div>
+ 
+                <div class="w-full lg:w-4/5">
+                    <h1 class="text-xl text-center lg:text-left text-blue-500">Shipping</h1>
+                    <p class="px-10 md:px-0 text-center md:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, repudiandae. Aspernatur quibusdam tempora reprehenderit modi quisquam, unde commodi laudantium, cupiditate neque tempore corporis! Possimus eius eum maxime iusto repudiandae veniam!</p>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <div class="flex justify-center w-full">
+        <div class="flex justify-center shrink-0 py-3 px-8 rounded-lg bg-tertiaryColor font-semibold text-white uppercase"><a href="#">Browse more vehicles</a></div>
+    </div>
+
 </template>

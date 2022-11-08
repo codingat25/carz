59ac1de6883811219ccd3beb6d2d938b5240e382
Christian Jay <christianquimporeyes@gmail.com>
2022-11-08 21:34:18 +0800

    a little better

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -5,57 +5,55 @@ import { ref } from 'vue';
 <template>
     <div class="flex flex-wrap w-full">
         <div class="flex justify-center items-center w-full h-auto">
-            <h1 class="text-xl md:text-3xl py-10 text-primaryColor font-black">
+            <h1 class="text-2xl md:text-3xl py-16 text-primaryColor font-semibold">
                 BUY AND SELL CARS MORE EASILY
             </h1>
         </div>
 
         
-        <div class="container mx-auto flex flex-col lg:flex-row w-full">
+        <div class="container mx-auto space-y-10 lg:space-y-0 space-x-0 lg:space-x-10 xl:space-x-16 flex flex-col lg:flex-row w-full pb-16">
 
-            <div class="flex flex-col md:flex-row justify-around w-full lg:w-1/3 h-auto pb-10">
+            <div class="flex flex-col md:flex-row justify-around w-full lg:w-1/3 h-auto">
                 <div class="flex justify-center w-full md:w-auto">
                     <div class="w-24 h-24 mb-5 md:mb-0 mr-0 md:mr-5  ">
-                      <img src="../assets/img/Payments.png" alt="shipping">
+                      <img src="../assets/img/Shipping.png" alt="shipping">
                      </div>
                 </div>
  
                 <div class="w-full lg:w-4/5">
-                    <h1 class="text-xl text-center lg:text-left text-blue-500">Shipping</h1>
-                    <p class="px-10 md:px-0 text-center md:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, repudiandae. Aspernatur quibusdam tempora reprehenderit modi quisquam, unde commodi laudantium, cupiditate neque tempore corporis! Possimus eius eum maxime iusto repudiandae veniam!</p>
+                    <h1 class="pb-3 text-2xl text-center font-semibold lg:text-left text-tertiaryColor">Shipping to any state</h1>
+                    <p class="px-10 md:px-0 text-center md:text-left">You may arrange interstate shipping in the US throguh a third-party shipping company of your choice or through our authorized representative. <span class="text-primaryColor"><a href=""> View shipping instructions »</a></span> </p>
                 </div>
             </div>
 
-            <div class="flex flex-col md:flex-row justify-around w-full lg:w-1/3 h-auto pb-10">
+            <div class="flex flex-col md:flex-row justify-around w-full lg:w-1/3 h-auto">
                 <div class="flex justify-center w-full md:w-auto">
-                    <div class="w-24 h-24 mb-5 md:mb-0 mr-0 md:mr-5">
-                      <img src="../assets/img/Payments.png" alt="shipping">
+                    <div class="w-24 h-24 mb-5 md:mb-0 mr-0 md:mr-5  ">
+                      <img src="../assets/img/Payments.png" alt="payments">
                      </div>
                 </div>
  
                 <div class="w-full lg:w-4/5">
-                    <h1 class="text-xl text-center lg:text-left text-blue-500">Shipping</h1>
-                    <p class="px-10 md:px-0 text-center md:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, repudiandae. Aspernatur quibusdam tempora reprehenderit modi quisquam, unde commodi laudantium, cupiditate neque tempore corporis! Possimus eius eum maxime iusto repudiandae veniam!</p>
+                    <h1 class="pb-3 text-2xl text-center font-semibold lg:text-left text-tertiaryColor">Payments</h1>
+                    <p class="px-10 md:px-0 text-center md:text-left">All payments are due with 2 business days after sale. Buyers from other states are highly recommended to add funds to their account ahead of time.  <span class="text-primaryColor"><a href="">View payment  instructions »</a></span> </p>
                 </div>
             </div>
 
-            <div class="flex flex-col md:flex-row justify-around w-full lg:w-1/3 h-auto pb-10">
+            <div class="flex flex-col md:flex-row justify-around w-full lg:w-1/3 h-auto">
                 <div class="flex justify-center w-full md:w-auto">
-                    <div class="w-24 h-24 mb-5 md:mb-0 mr-0 md:mr-5        ">
-                      <img src="../assets/img/Payments.png" alt="shipping">
+                    <div class="w-24 h-24 mb-5 md:mb-0 mr-0 md:mr-5  ">
+                      <img src="../assets/img/Import details.png" alt="import details">
                      </div>
                 </div>
  
                 <div class="w-full lg:w-4/5">
-                    <h1 class="text-xl text-center lg:text-left text-blue-500">Shipping</h1>
-                    <p class="px-10 md:px-0 text-center md:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, repudiandae. Aspernatur quibusdam tempora reprehenderit modi quisquam, unde commodi laudantium, cupiditate neque tempore corporis! Possimus eius eum maxime iusto repudiandae veniam!</p>
+                    <h1 class="pb-3 text-2xl text-center font-semibold lg:text-left text-tertiaryColor">Import Details</h1>
+                    <p class="px-10 md:px-0 text-center md:text-left">Visit  <span class="text-primaryColor"><a href="#">Your state customs website</a> </span>  to learn more about the laws and regulation of importing cars into your state.</p>
                 </div>
             </div>
+
         </div>
     </div>
 
-    <div class="flex justify-center w-full">
-        <div class="flex justify-center shrink-0 py-3 px-8 rounded-lg bg-tertiaryColor font-semibold text-white uppercase"><a href="#">Browse more vehicles</a></div>
-    </div>
 
 </template>

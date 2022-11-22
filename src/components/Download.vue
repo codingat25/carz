ab6645468518adf72264a7494d52c3e85240285f
Christian Jay <christianquimporeyes@gmail.com>
2022-11-22 17:09:55 +0800

    download

---
+++ src/components/Download.vue
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<template>
+    <div class="grid grid-cols-8">
+        <div class="col-start-3 col-span-4">
+            <div class="bg-indigo-900">
+
+            </div>
+        </div>
+    </div>
+</template>

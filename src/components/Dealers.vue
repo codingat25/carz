78a1bddd80f75aa3ca8cbf47b408e99eef6c4ae5
Christian Jay <christianquimporeyes@gmail.com>
2022-11-22 16:27:09 +0800

    dealers

--- src/components/Dealers.vue
+++ src/components/Dealers.vue
@@ -2,5 +2,45 @@
 </script>
 
 <template>
-    
+    <div class="flex flex-col w-auto h-auto pt-10">
+        <div class="flex justify-center items-center w-full pt-5">
+            <h1 class="text-cyan-700 font-bold text-base md:text-lg">OUR SATISFIED CUSTOMERS</h1>
+        </div>
+
+        <div class="grid grid-cols-8 py-10">
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-20 lg:gap-10 col-start-2 col-end-8 pt-10">
+                <div class="flex flex-col items-center h-auto shadow-gray-400 shadow-lg rounded-md pb-4">
+                    <div class="w-20 h-20 -mt-7">
+                        <img src="../assets/img/guy.png" alt="" class="rounded-full">
+                    </div>
+                    <div class="w-[75%] h-full py-4 ">
+                        <p class="text-xs text-gray-600 text-center pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in beatae quam illo, maiores soluta facere? Veniam vitae ipsum corrupti sequi architecto ullam unde fugit eius? At modi voluptatem expedita!</p>
+                    </div>
+                    <div class="text-semibold font-semibold text-gray-600">John Doe</div>
+                    <div class="text-xs text-gray-600">Manager</div>
+                </div>
+                <div class="flex flex-col items-center h-auto shadow-gray-400 shadow-lg rounded-md pb-4">
+                    <div class="w-20 h-20 -mt-7">
+                        <img src="../assets/img/guy.png" alt="" class="rounded-full">
+                    </div>
+                    <div class="w-[75%] h-full py-4 ">
+                        <p class="text-xs text-gray-600 text-center pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in beatae quam illo, maiores soluta facere? Veniam vitae ipsum corrupti sequi architecto ullam unde fugit eius? At modi voluptatem expedita!</p>
+                    </div>
+                    <div class="text-semibold font-semibold text-gray-600">John Doe</div>
+                    <div class="text-xs text-gray-600">Manager</div>
+                </div>
+                <div class="flex flex-col items-center h-auto shadow-gray-400 shadow-lg rounded-md pb-4">
+                    <div class="w-20 h-20 -mt-7">
+                        <img src="../assets/img/guy.png" alt="" class="rounded-full">
+                    </div>
+                    <div class="w-[75%] h-full py-4 ">  
+                        <p class="text-xs text-gray-600 text-center pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in beatae quam illo, maiores soluta facere? Veniam vitae ipsum corrupti sequi architecto ullam unde fugit eius? At modi voluptatem expedita!</p>
+                    </div>
+                    <div class="text-semibold font-semibold text-gray-600">John Doe</div>
+                    <div class="text-xs text-gray-600">Manager</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -2,39 +2,39 @@
 </script>
 
 <template>
-    <div class="flex flex-col w-auto h-auto">
-        <div class="flex justify-center items-center w-full h-1/5 pt-5">
+    <div class="flex flex-col w-auto h-auto pt-10">
+        <div class="flex justify-center items-center w-full pt-5">
             <h1 class="text-orange font-bold text-base md:text-lg">BUY AND SELL CARS EASILY</h1>
         </div>
 
-        <div class="grid grid-cols-8 h-4/5 items-center py-10">
-            <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 col-start-2 col-end-8">
-                <div class="flex h-auto">
-                    <div class="pt-4 pr-2 w-[25%] h-[25%] rounded-full ">
+        <div class="grid grid-cols-8 py-10">
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-5 col-start-2 col-end-8">
+                <div class="flex flex-col md:flex-row items-center lg:items-start h-auto">
+                    <div class="pt-4 pr-2 rounded-full ">
                         <img src="../assets/img/Shipping.png" alt="">
                     </div>
-                    <div class="w-[75%] h-full ">
+                    <div class="w-[80%] h-full text-center md:text-start">
                         <h3 class="text-cyan-700 font-semibold text-base">Shipping to any state</h3>
-                        <p class="text-xs text-gray-600 pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in beatae quam illo, maiores soluta facere? Veniam vitae ipsum corrupti sequi architecto ullam unde fugit eius? At modi voluptatem expedita!</p>
+                        <p class="text-xs text-gray-600 lg:text-left pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in beatae quam illo, maiores soluta facere? Veniam vitae ipsum corrupti sequi architecto ullam unde fugit eius? At modi voluptatem expedita!</p>
                     </div>
                 </div>
-                <div class="flex h-auto">
-                    <div class="pt-4 pr-2 w-[25%] h-[25%] rounded-full ">
+                <div class="flex flex-col md:flex-row items-center lg:items-start h-auto">
+                    <div class="pt-4 pr-2 rounded-full ">
                         <img src="../assets/img/Payments.png" alt="">
                     </div>
-                    <div class="w-[75%] h-full ">
+                    <div class="w-[80%] h-full text-center md:text-start">
                         <h3 class="text-cyan-700 font-semibold text-base">Payments</h3>
-                        <p class="text-xs text-gray-600 pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in beatae quam illo, maiores soluta facere? Veniam vitae ipsum corrupti sequi architecto ullam unde fugit eius? At modi voluptatem expedita!</p>
+                        <p class="text-xs text-gray-600 lg:text-left pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in beatae quam illo, maiores soluta facere? Veniam vitae ipsum corrupti sequi architecto ullam unde fugit eius? At modi voluptatem expedita!</p>
 
                     </div>
                 </div>
-                <div class="flex h-auto">
-                    <div class="pt-4 pr-2 w-[25%] h-[25%] rounded-full ">
+                <div class="flex flex-col md:flex-row items-center lg:items-start h-auto">
+                    <div class="pt-4 pr-2 rounded-full ">
                         <img src="../assets/img/Import details.png" alt="">
                     </div>
-                    <div class="w-[75%] h-full ">
+                    <div class="w-[80%] h-full text-center md:text-start">
                         <h3 class="text-cyan-700 font-semibold text-base">Import details</h3>
-                        <p class="text-xs text-gray-600 pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in beatae quam illo, maiores soluta facere? Veniam vitae ipsum corrupti sequi architecto ullam unde fugit eius? At modi voluptatem expedita!</p>
+                        <p class="text-xs text-gray-600 lg:text-left pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in beatae quam illo, maiores soluta facere? Veniam vitae ipsum corrupti sequi architecto ullam unde fugit eius? At modi voluptatem expedita!</p>
 
                     </div>
                 </div>

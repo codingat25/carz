6bcdd1c4fe9204e2d66e6bbe84dee3fa77b7bc18
Christian Jay <christianquimporeyes@gmail.com>
2022-12-03 22:22:19 +0800

    newsletter

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -2,15 +2,22 @@
 </script>
 
 <template>
-    <div class="flex justify-center">
-        <div class="">
-            <form action="">
-                <input type="text" >
-                <button class="w-30 h-auto bg-cyan-700">
-                    <a href="#">SUBSCRIBE</a>
-                </button>
-            </form>
-        </div>
+    <div class="flex flex-col justify-center items-center bg-lightBlue py-20 guide">
+
+
+            <div class="text-base text-center">
+                <h3> Subscribe to Carbid360 Newsletter</h3>
+            </div>
+            
+            <div class="flex flex-col lg:flex-row justify-center items-center w-full px-2 pt-4">
+                <form action="" class="flex justify-center items-center border-0 rounded-l-lg">
+                    <input type="text" placeholder="Email Address" class="w-3/4 py-3">
+                    <button class="border-0 rounded-r-lg w-2/4 py-2 bg-cyan-700">
+                        <a href="#" class="text-white">SUBSCRIBE</a>
+                    </button>
+                </form>
+            </div>
+
 
     </div>
 </template>
